c7d0e4f8ca650591549c45db2bac72763ae46048
ppp3ppj <ppp.3ppj@gmail.com>
2024-10-28 22:23:01 +0700

    add example for simple test and nagative space

--- src/nagative_space_programming/src/main.zig
+++ src/nagative_space_programming/src/main.zig
@@ -1,16 +1,23 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
-pub fn main() !void { }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    const width = 3;
+    const height = 2;
+    const result = calculateArea(width, height);
+    std.debug.print("result is {d}", .{result});
 }
 
-test "fuzz example" {
-    // Try passing `--fuzz` to `zig build` and see if it manages to fail this test case!
-    const input_bytes = std.testing.fuzzInput(.{});
-    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
+
+fn calculateArea(width: i32, height: i32) i32 {
+    assert(width > 0);
+    assert(height > 0);
+    return width * height;
+}
+
+test "test calculateArea with expected area value" {
+    const width = 3;
+    const height = 2;
+    const expected_area = 6;
+    try std.testing.expectEqual(expected_area, calculateArea(width, height));
 }
